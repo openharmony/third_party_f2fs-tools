911a77f16199179a0fcd9646284a55a86979e30f
yangbo <yangbo258@h-partners.com>
2026-02-28 10:31:16 +0800

    fixbug
    Signed-off-by: yangbo <yangbo258@h-partners.com>

--- fsck/fsck.h
+++ fsck/fsck.h
@@ -81,7 +81,7 @@ struct child_info {
 	u32 last_blk;
 	u32 i_namelen;  /* dentry namelen */
 	u32 verity_start_ofs; /* verity data position */
-	bool check_verity; /* start to check verity data, only userd in verity file */
+	bool check_verity; /* start to check verity data, only used in verity file */
 	bool is_dedup_out_inode;
 	bool is_unstable_inode;
 	bool is_verity_inode;
